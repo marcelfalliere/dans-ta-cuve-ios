3aa70c9e1aa2e4c92150f8267f83c3422e84d34b
Raphaël Mor <raphael.mor@gmail.com>
2013-09-26 10:38:34 +0200

    Initial import

---
+++ DansTaCuve/DansTaCuve/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DansTaCuve
+//
+//  Created by Raphaël on 26/09/13.
+//  Copyright (c) 2013 Dans ta cuve !. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RMAppDelegate class]));
+    }
+}
---
+++ DansTaCuve/DansTaCuveTests/DansTaCuveTests.m
@@ -0,0 +1,34 @@
+//
+//  DansTaCuveTests.m
+//  DansTaCuveTests
+//
+//  Created by Raphaël on 26/09/13.
+//  Copyright (c) 2013 Dans ta cuve !. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DansTaCuveTests : XCTestCase
+
+@end
+
+@implementation DansTaCuveTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
